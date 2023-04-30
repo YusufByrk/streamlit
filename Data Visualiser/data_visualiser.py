544d435c9e1b0112d28e7547d6226f1ac1194cc3
Yusuf Kenan Bayrak <107682742+YusufByrk@users.noreply.github.com>
2023-04-30 23:17:02 +0200

    20230430

--- Data Visualiser/data_visualiser.py
+++ Data Visualiser/data_visualiser.py
@@ -30,11 +30,17 @@ if files:
         if option != "None":
             for file in files:
                 if file.name in selected_files:
-                    data = pd.read_excel(file, sheet_name="Rapor") # index olarak atamak istediğimiz bir sütun olursa index_col = 0 fonksiyonunu kullanabiliriz.
+                    data = pd.read_excel(file, index_col=0) # index olarak atamak istediğimiz bir sütun olursa index_col = 0 fonksiyonunu kullanabiliriz.
                     item = list(data[option])
-                    dates = date_converter(data["date"])
+                    dates = date_converter(data["DATE"])
                     index = np.arange(len(dates))
-                    plt.gfc().autofmt_xdate()
-                    plt.plot(index, item)
+                    plt.xticks(index, dates)
+                    plt.gcf().autofmt_xdate()
+                    plt.plot(index, item, label=file.name, marker='o')
+                    plt.xlabel("Date")
+                    plt.ylabel(option)
+                    plt.title(option + " Chart")
+                    plt.grid(True)
+                    plt.legend()
             st.write(figure)
         
